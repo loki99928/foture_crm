0876ad7d3e6bc2455ed3a3e35ddca0f9ccf84458
Babanin_DV <loki99928@yandex.ru>
2023-03-20 20:27:54 +0700

    поле поиска

--- client/src/templates/components/menu/headerMineMenu/HeaderMineMenu.tsx
+++ client/src/templates/components/menu/headerMineMenu/HeaderMineMenu.tsx
@@ -6,12 +6,14 @@ import {UserMenu} from "../userMenu/UserMenu";
 import {SettingsMenu} from "../settingsMenu/SettingsMenu";
 import {AlarmElement} from "../../elements/alarm/Alarm.element";
 import {MessageElement} from "../../elements/message/Message.element";
+import {SearchElement} from "../../elements/search/Search.element";
 
 export const HeaderMineMenu = () => {
 
     return (
         <React.Fragment>
             <div className={cn(s.containerMenu, s.container__menu)}>
+                <SearchElement/>
                 <MessageElement/>
                 <AlarmElement/>
                 <UserMenu/>
