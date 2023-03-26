04c8e77cc0dfc2bab5aed02580cba727a63440ac
Babanin_DV <loki99928@yandex.ru>
2023-03-26 16:50:55 +0700

    Тестировани. Исправление кнопки Send

--- client/src/templates/components/forms/FomControls/Button.tsx
+++ client/src/templates/components/forms/FomControls/Button.tsx
@@ -4,15 +4,15 @@ import s from "./Form.module.scss";
 
 export const Button: React.FC<any> = props => {
     const {disabled, ...rest} = props
-  return (
-      <div>
-          <button className={cn(s.formBtn, s.formBtn_hover, s.formBtn_active, s.formBtn_focus)}
-                  disabled={disabled}
-                  type="submit"
-                  {...rest}
-          >
-              submit
-          </button>
-      </div>
-  )
+    return (
+        <div>
+            <button className={cn(s.formBtn, s.formBtn_hover, s.formBtn_active, s.formBtn_focus)}
+                    disabled={disabled}
+                    type="submit"
+                    {...rest}
+            >
+                Send
+            </button>
+        </div>
+    )
 }
